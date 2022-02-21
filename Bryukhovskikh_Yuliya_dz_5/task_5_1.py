f799a034d5167b738249fe290efb7126c2e9d2de
Yuliya Bryukhovskikh <makutu@ereb.ru>
2022-02-22 00:41:10 +0300

    откат лишних исправлений

--- Bryukhovskikh_Yuliya_dz_5/task_5_1.py
+++ Bryukhovskikh_Yuliya_dz_5/task_5_1.py
@@ -12,6 +12,5 @@ def odd_nums(number: int) -> Generator[int, None, None]:
 n = 15
 generator = odd_nums(n)
 for _ in range(1, n + 1, 2):
-    print(next(generator), end=' ')
-print()
+    print(next(generator))
 # next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
