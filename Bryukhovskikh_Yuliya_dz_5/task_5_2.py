0683aa6df5dc39e941e7f0c7f6bde14cd674a8d0
Yuliya Bryukhovskikh <makutu@ereb.ru>
2022-02-21 16:49:14 +0300

    задание 4 в пятом уроке, с ошибкой

--- Bryukhovskikh_Yuliya_dz_5/task_5_2.py
+++ Bryukhovskikh_Yuliya_dz_5/task_5_2.py
@@ -3,7 +3,7 @@ from typing import Generator
 
 def odd_nums(number: int) -> Generator[int, None, None]:
     """Генератор, возвращающий по очереди нечетные целые числа от 1 до number (включительно) без yield"""
-    return (i for i in range(1, n+1, 2))
+    return (i for i in range(1, number+1, 2))
 
 
 n = 15
