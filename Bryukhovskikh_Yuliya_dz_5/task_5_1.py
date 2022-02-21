06d81c6069f384bd501140d752424fec5cba4417
Yuliya Bryukhovskikh <makutu@ereb.ru>
2022-02-21 16:01:26 +0300

    задание 1 в пятом уроке

---
+++ Bryukhovskikh_Yuliya_dz_5/task_5_1.py
@@ -0,0 +1,16 @@
+from typing import Generator
+
+
+def odd_nums(number: int) -> Generator[int, None, None]:
+    """Генератор, возвращающий по очереди нечетные целые числа от 1 до number (включительно)"""
+    i = 1
+    while i <= number:
+        yield i
+        i += 2
+
+
+n = 15
+generator = odd_nums(n)
+for _ in range(1, n + 1, 2):
+    print(next(generator))
+# next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
