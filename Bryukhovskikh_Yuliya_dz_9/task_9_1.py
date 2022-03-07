4e53934e5dc66bafc83394bc95a7fe2231b1f1d7
Yuliya Bryukhovskikh <makutu@ereb.ru>
2022-03-07 17:28:12 +0300

    задание 1 девятого урока

--- Bryukhovskikh_Yuliya_dz_9/task_9_1.py
+++ Bryukhovskikh_Yuliya_dz_9/task_9_1.py
@@ -1,8 +1,23 @@
+from time import sleep
+
+
 class TrafficLight:
-    ...
+    """
+    Так настроен светофор
+    """
+
+    __color = (
+        {'color': 'red', 'time': 4},
+        {'color': 'yellow', 'time': 2},
+        {'color': 'green', 'time': 3},
+    )
 
     def running(self):
-        ...
+        for el in self.__color:
+            # red 4 сек
+            print(f"{el['color']} {el['time']} сек")
+            sleep(el['time'])
+
 
 
 if __name__ == '__main__':
